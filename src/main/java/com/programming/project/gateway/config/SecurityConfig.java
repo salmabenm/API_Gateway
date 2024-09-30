f6d83e392b9bd05b54bd07bdd96865706a48c46a
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2024-09-30 21:20:41 +0100

    Add Web Config

--- src/main/java/com/programming/project/gateway/config/SecurityConfig.java
+++ src/main/java/com/programming/project/gateway/config/SecurityConfig.java
@@ -4,21 +4,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-            "/swagger-resources/**", "/api-docs/**","/aggregate/**"};
+    private final String[] freeResourceUrls = {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/api-docs/**",
+            "/aggregate/**"
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(freeResourceUrls)
+        httpSecurity
+                .cors() // Enable CORS
+                .and()
+                .csrf().disable() // Disable CSRF if necessary
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls) // Allow free URLs
                         .permitAll()
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .build();
+                        .anyRequest().authenticated() // Requires authentication for all other requests
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())); // Configure OAuth2 resource server
+
+        return httpSecurity.build(); // Build and return security filter
     }
+
 }
