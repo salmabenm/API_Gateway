f7140ba585e0d6cc41150619364fdf9fdae2fe67
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2024-10-04 19:45:35 +0100

    Observability with Grafana Stack

--- src/main/java/com/programming/project/gateway/config/SecurityConfig.java
+++ src/main/java/com/programming/project/gateway/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-resources/**",
             "/api-docs/**",
-            "/aggregate/**"
+            "/aggregate/**",
+            "/actuator/prometheus"
     };
 
     @Bean
